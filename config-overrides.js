25686d2b8f62b32f04698ef5b7fa8f8c0f96e5ba
Rowan Carmichael <carmichaelr@hotmail.co.uk>
2018-06-24 18:13:23 +1200

    Adding assets

--- config-overrides.js
+++ config-overrides.js
@@ -1,8 +1,10 @@
 /* config-overrides.js */
 
 const rewireCSS = require('./rewire/react-app-rewire-css-scss');
+const rewireSvgReactLoader = require('react-app-rewire-svg-react-loader');
 
 module.exports = function override(config, env) {
   config = rewireCSS(config, env);
+  config = rewireSvgReactLoader(config, env);
   return config;
 };
